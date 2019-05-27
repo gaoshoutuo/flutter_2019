dc57a5d77477d9fe48ae33ff9629fd9ee9f81adb
zth <715686280@qq.com>
2019-05-27 20:07:54 +0800

    627

--- lib/HelloWorld.dart
+++ lib/HelloWorld.dart
@@ -1,2 +1,12 @@
 import 'package:flutter/material.dart';
 
+class testWidget extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return null;
+  }
+
+  //而且因为是状态还需要状态的内部类
+
+}
